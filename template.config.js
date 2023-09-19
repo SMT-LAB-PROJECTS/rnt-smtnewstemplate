3f69b9ec137af87537d0676742f8bf83edd7293a
tarunsmtgroup <59910067+tarunsmtgroup@users.noreply.github.com>
2023-09-19 17:59:01 +0530

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "smtnewstemplate",
+  placeholderName: "SmtNewsTemplate",
 
   // Directory with template
   templateDir: "./template",
